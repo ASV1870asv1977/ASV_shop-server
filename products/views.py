e804d0e92a00eb269b72796ca6b131947d896d11
ASV1870asv1977 <73542571+ASV1870asv1977@users.noreply.github.com>
2021-07-05 11:08:09 +0300

    lesson_2 tasks added

--- products/views.py
+++ products/views.py
@@ -17,21 +17,11 @@ def products(request):
     ''' Функция - контроллер на отображение шаблона products.html '''
     context = {
         'title': 'GeekShop - Каталог',
-        #'products': json_read_product(),
         'categories': ProductCategory.objects.all(),
         'products': Product.objects.all(),
     }
     return render(request, 'products/products.html', context)
 
-
-'''
-def json_read_product():
-     Функция чтения данных по продуктам из файла products.json
-
-    with open('products/fixtures/products.json', 'r', encoding='UTF-8') as f:
-        products_list = json.load(f)
-    return products_list
-'''
 
 
 
