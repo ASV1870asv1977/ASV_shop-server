2f726e5bb9eb776a233d14b7a2cbf1c59a5eacf9
ASV1870asv1977 <73542571+ASV1870asv1977@users.noreply.github.com>
2021-07-05 11:03:11 +0300

    added work with models in controllers and templates

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import json
+from products.models import Product, ProductCategory
 # Create your views here.
 # функции = контроллеры = въюхи
 
@@ -16,18 +17,21 @@ def products(request):
     ''' Функция - контроллер на отображение шаблона products.html '''
     context = {
         'title': 'GeekShop - Каталог',
-        'products': json_read_product(),
+        #'products': json_read_product(),
+        'categories': ProductCategory.objects.all(),
+        'products': Product.objects.all(),
     }
     return render(request, 'products/products.html', context)
 
 
+'''
 def json_read_product():
-    ''' Функция чтения данных по продуктам из файла products.json'''
+     Функция чтения данных по продуктам из файла products.json
 
     with open('products/fixtures/products.json', 'r', encoding='UTF-8') as f:
         products_list = json.load(f)
     return products_list
-
+'''
 
 
 
