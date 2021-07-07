c4acbab193080fd79877d6a3e3aeb2bc45c741a1
ASV1870asv1977 <73542571+ASV1870asv1977@users.noreply.github.com>
2021-07-07 17:44:18 +0300

    created directory user_images and added user images

--- users/models.py
+++ users/models.py
@@ -7,3 +7,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True)
+    # В базе все необходимые данные прописаны в соответствии с наследованием
