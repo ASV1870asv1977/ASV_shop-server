f4f95699e37eb4576b4c706752d917bae54170f5
ASV1870asv1977 <73542571+ASV1870asv1977@users.noreply.github.com>
2021-07-07 11:50:02 +0300

    added controllers 'login' and 'registration' for users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def login(request):
+    ''' Функция - контроллер на отображение шаблона login.html '''
+
+    return render(request, 'users/login.html')
+
+
+def registration(request):
+    ''' Функция - контроллер на отображение шаблона registration.html '''
+
+    return render(request, 'users/registration.html')
